a46f0701cfa73b1acb325dcceeeb5c97a1af6dd2
782336 <782336@PCIN475082.cts.com>
2019-09-12 16:49:53 +0530

    pojo modified

--- src/main/java/com/cts/model/Order.java
+++ src/main/java/com/cts/model/Order.java
@@ -1,44 +1,50 @@
 package com.cts.model;
 
-import java.util.Date;
-
 public class Order {
 
-	private int orderId;
-	private int prodId;
-	private int userID;
-	private Date orderDate;
+	private String orderId;
+	private String prodId;
+	private String userID;
+	private String orderDate;
 
-	public int getOrderId() {
+	public String getOrderId() {
 		return orderId;
 	}
 
-	public void setOrderId(int orderId) {
+	public void setOrderId(String orderId) {
 		this.orderId = orderId;
 	}
 
-	public int getProdId() {
+	public String getProdId() {
 		return prodId;
 	}
 
-	public void setProdId(int prodId) {
+	public void setProdId(String prodId) {
 		this.prodId = prodId;
 	}
 
-	public int getUserID() {
+	public String getUserID() {
 		return userID;
 	}
 
-	public void setUserID(int userID) {
+	public void setUserID(String userID) {
 		this.userID = userID;
 	}
 
-	public Date getOrderDate() {
+	public String getOrderDate() {
 		return orderDate;
 	}
 
-	public void setOrderDate(Date orderDate) {
+	public void setOrderDate(String orderDate) {
 		this.orderDate = orderDate;
 	}
+
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", prodId=" + prodId + ", userID=" + userID + ", orderDate=" + orderDate
+				+ "]";
+	}
+	
+	
 
 }
