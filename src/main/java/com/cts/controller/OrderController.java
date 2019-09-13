3f00c7ddae340196db778e9d262ff9fd4900c1d9
782336 <782336@PCIN475082.cts.com>
2019-09-13 10:00:35 +0530

    Some changes for url mapping

--- src/main/java/com/cts/controller/OrderController.java
+++ src/main/java/com/cts/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,33 +18,34 @@ import com.cts.service.OrderService;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-	
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@PutMapping("/")
+	@PostMapping
 	public ResponseEntity<Object> placeOrder(@RequestBody Order order) {
 		Order savedOrder = orderService.placeOrder(order);
-		if(savedOrder != null) {
-			return new ResponseEntity("Order placed successfully", HttpStatus.OK);
+		if (savedOrder != null) {
+			return new ResponseEntity("Order placed successfully. Order id is:"+order.getOrderId(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity("Unable to place order", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@GetMapping(value="/cancel/{orderId}")
+	@GetMapping(value = "/cancel/{orderId}")
 	public ResponseEntity<Object> cancelOrder(@PathVariable("orderId") String orderId) {
 		Order order = orderService.cancelOrder(orderId);
-		if(order == null) {
+		if (order == null) {
 			return new ResponseEntity("Order not found in the system", HttpStatus.NOT_FOUND);
 		} else {
-			return new ResponseEntity("Order with orderId "+order.getOrderId()+" cancelled", HttpStatus.OK);
+			return new ResponseEntity("Order with orderId : " + orderId + " cancelled successfully", HttpStatus.OK);
 		}
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@GetMapping(value = "/")
+	@GetMapping
 	public ResponseEntity<List<Order>> getAllOrders() {
 		List<Order> orders = orderService.getAllOrders();
 		return new ResponseEntity(orders, HttpStatus.OK);
