5a27d4a54160944ac378bac2727463e6a82a0faa
764432 <764432@PC329843.cts.com>
2019-09-16 17:32:49 +0530

    "added unit test"

--- src/test/java/com/cts/controller/ProductDetailsControllerTest.java
+++ src/test/java/com/cts/controller/ProductDetailsControllerTest.java
@@ -5,16 +5,26 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 
 import com.cts.model.Product;
 import com.cts.repository.ProductDetailssRepo;
@@ -35,22 +45,34 @@ public class ProductDetailsControllerTest extends AbstractTest {
 	@Mock
 	private ProductDetailssRepo productDetailssRepo;
 
+	@Mock
+	private ProductDetailsService service;
+	
+	@Mock
+    private RestTemplate restTemplate;
+	
+	@Before
+	@Override
+	public void setUp() {
+		super.setUp();
+	}
+
 	@Test
 	public void testAddItem() {
 		File file;
 		file = new File("./src/main/resources/excel/product.xlsx");
 		Product product = new Product();
-		product.setProdId("1");
+		product.setProdId("112");
 		product.setProdName("TV");
 		product.setPrice("1000");
-		String Response = "Item added Successfully";
+		String Response = "Product Added Successfully";
 		when(productDetailsService.addItem(product)).thenReturn(Response);
 		productDetailsController.addItem(product);
 	}
 
 	@Test
 	public void getProductByIdSuccess() throws Exception {
-		String uri = "/products/123";
+		String uri = "/products/12";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
 				.andReturn();
 
@@ -68,4 +90,15 @@ public class ProductDetailsControllerTest extends AbstractTest {
 		assertEquals(HttpStatus.NOT_FOUND.value(), status);
 	}
 
+	@Test
+	public void testRemoveItem() {
+		Product product = new Product();
+		product.setProdId("1");
+		product.setProdName("A");
+		product.setPrice("10000");
+		when(service.removeItem(product.getProdId())).thenReturn("Removed");
+		productDetailsController.removeItem(product.getProdId());
+		}
+	
+	
 }
