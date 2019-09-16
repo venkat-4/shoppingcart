dfbb420bf0ff83eabbf54121c9c8d6febf3f946c
788599 <788599@PCIN474771.cts.com>
2019-09-16 17:12:57 +0530

    Custom Exception class added two methods

--- src/test/java/com/cts/exception/InternalServerErrorTest.java
+++ src/test/java/com/cts/exception/InternalServerErrorTest.java
@@ -2,6 +2,8 @@ package com.cts.exception;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,10 +13,13 @@ public class InternalServerErrorTest {
 
 	@Mock
 	InternalServerError internalServer;
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	public InternalServerErrorTest() {
 		internalServer = new InternalServerError("INTERNAL_SERVER_ERROR");
-
+		exception.expect(InternalServerError.class);
+		exception.expectMessage("InternalServerException");
 		assertNotNull(internalServer);
 
 	}
--- src/test/java/com/cts/exception/RecordNotFoundExceptionTest.java
+++ src/test/java/com/cts/exception/RecordNotFoundExceptionTest.java
@@ -2,6 +2,8 @@ package com.cts.exception;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,10 +13,14 @@ public class RecordNotFoundExceptionTest {
 
 	@Mock
 	RecordNotFoundException recordNotFoundException;
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	public RecordNotFoundExceptionTest() {
 
 		recordNotFoundException = new RecordNotFoundException("RecordNotFoundException");
+		exception.expect(InternalServerError.class);
+		exception.expectMessage("recordNotFoundException");
 		assertNotNull(recordNotFoundException);
 	}
 
