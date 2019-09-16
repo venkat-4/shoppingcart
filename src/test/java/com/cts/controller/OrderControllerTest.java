1525a057b5a61ac805fffa7f81823e37281609ad
Chandra <782336@cognizant.com>
2019-09-16 11:46:13 +0530

    order test done

--- src/test/java/com/cts/controller/OrderControllerTest.java
+++ src/test/java/com/cts/controller/OrderControllerTest.java
@@ -2,19 +2,23 @@ package com.cts.controller;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 
 import com.cts.model.Order;
+import com.cts.repository.OrderRepository;
 import com.cts.repository.ProductDetailssRepo;
 import com.cts.service.OrderService;
 import com.cts.util.RWExcelOrder;
@@ -41,35 +45,55 @@ public class OrderControllerTest extends AbstractTest {
 
 	@Test
 	public void testAddOrder() throws Exception {
-		String uri = "/orders";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).accept(MediaType.APPLICATION_JSON_VALUE))
-				.andReturn();
-		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
-		String content = mvcResult.getResponse().getContentAsString();
-		Order order = (Order) super.mapFromJson(content, Order.class);
-		assertTrue(order != null);
+		String url = "http://localhost:9090/orders";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Order order = new Order();
+		order.setOrderId("OR-123");
+		order.setOrderDate("2019-07-11");
+		order.setProdId("PROD-123");
+		order.setUserID("USR-123");
+		String inputJson = super.mapToJson(order);
+		HttpEntity<String> request = new HttpEntity<String>(inputJson, headers);
+		String response = restTemplate.postForObject(url, request, String.class);
+		assertEquals(true, response.contains("Order placed successfully"));
 	}
 
 	@Test
 	public void testCancelOrderSuccess() throws Exception {
-		String uri = "/orders/cancel/789";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
-				.andReturn();
+
+		String postUrl = "http://localhost:9090/orders";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Order order = new Order();
+		order.setOrderId("OR-123");
+		order.setOrderDate("2019-07-11");
+		order.setProdId("PROD-123");
+		order.setUserID("USR-123");
+		String inputJson = super.mapToJson(order);
+		HttpEntity<String> request = new HttpEntity<String>(inputJson, headers);
+		String response = restTemplate.postForObject(postUrl, request, String.class);
+		assertEquals(true, response.contains("Order placed successfully"));
+
+		String cancelUrl = "/orders/cancel/OR-123";
+		MvcResult mvcResult = mvc
+				.perform(MockMvcRequestBuilders.get(cancelUrl).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 		assertNotNull(mvcResult.getResponse());
 		int status = mvcResult.getResponse().getStatus();
-		
+
 		assertEquals(HttpStatus.OK.value(), status);
+
 	}
-	
+
 	@Test
 	public void testCancelOrderFailure() throws Exception {
-		String uri = "/orders/cancel/345";
+		String uri = "/orders/cancel/xyz123";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
 				.andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
 		assertEquals(HttpStatus.NOT_FOUND.value(), status);
 	}
 
@@ -86,6 +110,7 @@ public class OrderControllerTest extends AbstractTest {
 		assertNotNull(mvcResult.getResponse());
 		assertTrue(orderlist.length > 0);
 	}
+
 	@Test
 	public void testGetAllOrdersFailure() throws Exception {
 		String uri = "/orders";
@@ -100,17 +125,31 @@ public class OrderControllerTest extends AbstractTest {
 
 	@Test
 	public void testGetOrderSuccess() throws Exception {
-		String uri = "/orders/123";
+		String url = "http://localhost:9090/orders";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Order order = new Order();
+		order.setOrderId("OR-123");
+		order.setOrderDate("2019-07-11");
+		order.setProdId("PROD-123");
+		order.setUserID("USR-123");
+		String inputJson = super.mapToJson(order);
+		HttpEntity<String> request = new HttpEntity<String>(inputJson, headers);
+		String response = restTemplate.postForObject(url, request, String.class);
+		assertEquals(true, response.contains("Order placed successfully"));
+
+		String uri = "/orders/OR-123";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
 				.andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		String content = mvcResult.getResponse().getContentAsString();
 		assertEquals(HttpStatus.OK.value(), status);
 	}
+
 	@Test
 	public void testGetOrderFailure() throws Exception {
-		String uri = "/orders/123";
+		String uri = "/orders/or-abc-123";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
 				.andReturn();
 
