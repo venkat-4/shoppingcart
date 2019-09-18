9359d7284c2a0e7ebf2307e0d5b619a73c08b4f1
788599 <788599@PCIN474771.cts.com>
2019-09-18 18:30:21 +0530

    PMD violation code change has been added

--- src/main/java/com/cts/exception/ErrorResponse.java
+++ src/main/java/com/cts/exception/ErrorResponse.java
@@ -2,14 +2,33 @@ package com.cts.exception;
 
 import java.util.List;
 
+/**
+ * EroorResponse need to be declared in a method or constructor's {@code throws}
+ * clause if they can be thrown by the execution of the method or constructor
+ * and propagate outside the method or constructor boundary.
+ * 
+ * @author 788599
+ *
+ */
 public class ErrorResponse {
 
 	private String message;
 	private List<String> details;
 
+	/**
+	 * Constructs a new ErrorResponse with {@code null} as its detail message.
+	 * The cause is not initialized, and may subsequently be initialized by a
+	 * call to {@link #initCause}.
+	 */
 	public ErrorResponse() {
 	}
 
+	/**
+	 * Parameterized Constructs a new ErrorResponse with as its detail message.
+	 * 
+	 * @param message
+	 * @param details
+	 */
 	public ErrorResponse(String message, List<String> details) {
 		this.message = message;
 		this.details = details;
--- src/main/java/com/cts/exception/RecordNotFoundException.java
+++ src/main/java/com/cts/exception/RecordNotFoundException.java
@@ -3,12 +3,28 @@ package com.cts.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Unchecked exceptions do <em>not</em> need to be declared in a method or
+ * constructor's {@code throws} clause if they can be thrown by the execution of
+ * the method or constructor and propagate outside the method or constructor
+ * boundary.
+ * 
+ * @author 788599
+ *
+ */
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class RecordNotFoundException  extends RuntimeException {
+public class RecordNotFoundException extends RuntimeException {
 
+	/**
+	 * Constructs a new runtime exception with the specified detail message.
+	 * 
+	 * @param message
+	 *            the detail message. The detail message is saved for later
+	 *            retrieval by the method.
+	 */
 	public RecordNotFoundException(String message) {
 		super(message);
 	}
-	
 
 }
