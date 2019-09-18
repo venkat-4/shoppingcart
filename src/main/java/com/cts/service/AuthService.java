a491f237d22b1242bae430162ab8f6fc17af1d58
764432 <764432@PC329843.cts.com>
2019-09-18 10:47:37 +0530

    Added Java doc for service layer.

--- src/main/java/com/cts/service/AuthService.java
+++ src/main/java/com/cts/service/AuthService.java
@@ -1,3 +1,9 @@
+/**
+ * This class is used to validate the authenticated user.
+ * 
+ * @author 764432
+ *
+ */
 package com.cts.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +18,24 @@ public class AuthService {
 	@Autowired
 	private AuthRepository authFileRepo;
 
+	/**
+	 * It is used to allow the user to login.
+	 * 
+	 * @param user
+	 * @return
+	 */
 	public String login(User user) {
 
 		return authFileRepo.login(user);
 
 	}
 
+	/**
+	 * It is used to create a user.
+	 * 
+	 * @param user
+	 * @return
+	 */
 	public String createUser(User user) {
 
 		return authFileRepo.createUser(user);
--- src/main/java/com/cts/service/OrderService.java
+++ src/main/java/com/cts/service/OrderService.java
@@ -1,3 +1,9 @@
+/**
+ * This class is used to give the order details.
+ * 
+ * @author 764432
+ *
+ */
 package com.cts.service;
 
 import java.util.List;
@@ -10,29 +16,58 @@ import com.cts.repository.OrderRepository;
 
 @Service("orderService")
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
+	/**
+	 * It is used to place the order.
+	 * 
+	 * @param order
+	 * @return
+	 */
 	public Order placeOrder(Order order) {
 		return orderRepository.placeOrder(order);
-		
+
 	}
 
+	/**
+	 * It is used to cancel the order.
+	 * 
+	 * @param orderId
+	 * @return
+	 */
 	public String cancelOrder(String orderId) {
 		return orderRepository.cancelOrder(orderId);
 	}
 
+	/**
+	 * It is used to getting the list of orders.
+	 * 
+	 * @return
+	 */
 	public List<Order> getAllOrders() {
 		return orderRepository.getAllOrders();
 	}
 
+	/**
+	 * It is used to get the product detail.
+	 * 
+	 * @param orderId
+	 * @return
+	 */
 	public Order getOrderById(String orderId) {
 		return orderRepository.getOrderById(orderId);
 	}
-	
+
+	/**
+	 * It is used to save the order details into excel.
+	 * 
+	 * @param placeOrder
+	 * @return
+	 */
 	public String save(Order placeOrder) {
 		return orderRepository.save(placeOrder);
 	}
-	
+
 }
